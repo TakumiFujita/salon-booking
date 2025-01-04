24507c3ca1af5fecb277a0fa16bd6fbc538d4338
TakumiFujita <46127387+TakumiFujita@users.noreply.github.com>
2025-01-04 12:34:18 +0900

    スタイリストhome画面に予約状況を表示

--- app/Http/Controllers/StylistController.php
+++ app/Http/Controllers/StylistController.php
@@ -2,13 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Reservation;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Log;
 
 class StylistController extends Controller
 {
     public function home()
     {
-        return view('stylist.home'); // ログインしていないユーザーのみがアクセス可能
+        $now = Carbon::now();
+        $todayReservations = Reservation::where('stylist_id', Auth::guard('stylist')->id())->whereDate('start_time', $now->format('Y-m-d'))->get();
+
+        return view('stylist.home', compact('now', 'todayReservations'));
     }
 }
--- resources/views/stylist/home.blade.php
+++ resources/views/stylist/home.blade.php
@@ -7,7 +7,19 @@
         </div>
     @endif
     <div class="container py-5">
-        <h3>本日のご予約状況</h3>
+        <h3>本日（{{ $now->isoFormat('YYYY年MM月DD日') }}）のご予約状況</h3>
+        @if ($todayReservations->isNotEmpty())
+            @foreach ($todayReservations as $reservation)
+                <tr>
+                    <td>
+                        ・{{ $reservation->start_time->isoFormat('HH時mm分') }}〜：{{ $reservation->user->name }}様（{{ $reservation->service->name }}）&nbsp;
+                    </td>
+                </tr>
+                @unless ($loop->last)
+                    <br>
+                @endunless
+            @endforeach
+        @endif
     </div>
     <form id="logout-form" action="{{ route('stylist.logout') }}" method="POST">
         @csrf
