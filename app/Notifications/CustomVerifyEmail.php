da05cce53481dadfe398b739c9cc613c560807db
TakumiFujita <46127387+TakumiFujita@users.noreply.github.com>
2025-01-18 12:42:05 +0900

    認証メールの日本語化

---
+++ app/Notifications/CustomVerifyEmail.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Lang;
+use Illuminate\Support\Facades\URL;
+
+class CustomVerifyEmail extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public static $toMailCallback;
+    public function __construct() {}
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        $verificationUrl = $this->verificationUrl($notifiable);
+
+        if (static::$toMailCallback) {
+            return call_user_func(static::$toMailCallback, $notifiable, $verificationUrl);
+        }
+
+        return (new MailMessage)
+            ->subject(Lang::get('メールアドレスの確認'))
+            ->line(Lang::get('以下のボタンをクリックして、メールアドレスを確認してください。'))
+            ->action(Lang::get('メールアドレスを確認する'), $verificationUrl);
+    }
+
+    /**
+     * Get the verification URL for the given notifiable.
+     *
+     * @param  mixed  $notifiable
+     * @return string
+     */
+    protected function verificationUrl($notifiable)
+    {
+        return URL::temporarySignedRoute(
+            'verification.verify',
+            Carbon::now()->addMinutes(Config::get('auth.verification.expire', 60)),
+            [
+                'id' => $notifiable->getKey(),
+                'hash' => sha1($notifiable->getEmailForVerification()),
+            ]
+        );
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ lang/ja/auth.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'failed' => '認証情報が記録と一致しません。',
+    'password' => '提供されたパスワードが正しくありません。',
+    'throttle' => ':seconds秒後に再試行してください。',
+    'verify_email_subject' => 'メールアドレスの確認',
+    'verify_email_line_1' => '以下のボタンをクリックして、メールアドレスを確認してください。',
+    'verify_email_action' => 'メールアドレスを確認する',
+    'verify_email_line_2' => '',
+    'regards' => 'どうぞ、よろしくお願い致します',
+    'action_trouble' => '":actionText" ボタンをクリックできない場合は、こちらのURLをコピーしてブラウザに貼り付けてください: [:actionURL]',
+];
